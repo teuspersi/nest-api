ca732f9c5b10fc9da72ce80b5730ac5d218ad9b2
Mateus <teuspersi@gmail.com>
2021-08-30 18:08:51 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(`${process.env.MONGO_CONNECT}`),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
